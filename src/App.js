b39a52441afcd8adce528c196576fae733c80ee0
Ignacio Ainol <iggnaxios@gmail.com>
2024-02-08 21:59:27 -0300

    Component to change Languaje desing done

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
 
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Form, Row } from 'react-bootstrap';
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { HomeScreen } from './screens/HomeScreen';
 import { ChangeLanguaje } from './components/ChangeLanguaje';
+import { EnvelopeAt } from 'react-bootstrap-icons';
 
 function App() {
   return (
     <BrowserRouter>
-      <Container>
-        <Row xs={2} md={4} lg={6}>
-          <Col>1 of 2</Col>
-          <Col>2 of 2</Col>
-        </Row>
-        <Row xs={1} md={2}>
-          <Col>1 of 3</Col>
-          <Col>2 of 3</Col>
-          <Col>3 of 3</Col>
-        </Row>
-        <Row xs="auto">
-          <Col>1 of 3</Col>
-          <Col>2 of 3</Col>
-          <Col>3 of 3</Col>
-        </Row>
-      </Container>
-      <main>
-        <Container className="mt-3">
-          <Routes>
-            <Route>
-              <Route path="/" element={<HomeScreen />}></Route>
-            </Route>
-          </Routes>
-        </Container>
-      </main>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Container className='mt-4'>
+            <ChangeLanguaje />
+            <Row className='mt-4'>
+              <Col>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'end' }}>
+                  <span className='space-between-item'>
+                    <EnvelopeAt /> ingenieria@mazarot.cl
+                  </span>
+                  <span className='space-between-item'>
+                    <EnvelopeAt /> contacto@mazarot.cl
+                  </span>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route>
+                <Route path="/" element={<HomeScreen />}></Route>
+              </Route>
+            </Routes>
+          </Container>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
--- src/components/ChangeLanguaje.js
+++ src/components/ChangeLanguaje.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Col, Form, Row } from 'react-bootstrap'
 
 export const ChangeLanguaje = () => {
     return (
-        <div>ChangeLanguaje</div>
+        <>
+            <Row>
+                <Col>
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'end' }}>
+                        <span className='flag'>
+                            <img src="./assets/images/chile.png" alt="chile_flag" />
+                        </span>
+                        <Form>
+                            <Form.Check // prettier-ignore
+                                type="switch"
+                                id="custom-switch"
+                            />
+                        </Form>
+
+                        <span className='flag'>
+                            <img src="./assets/images/uk.png" alt="chile_flag" />
+                        </span>
+                    </div>
+                </Col>
+            </Row>
+        </>
     )
 }
