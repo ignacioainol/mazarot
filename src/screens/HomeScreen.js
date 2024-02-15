c9f0e4a50fa20e456ac392ac1ad5d543942fd880
Ignacio Ainol <iggnaxios@gmail.com>
2024-02-14 23:35:18 -0300

    Divisor done

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Carousel } from 'react-bootstrap'
 import { ItemCarousel } from '../components/ItemCarousel'
 
+
 export const HomeScreen = () => {
     return (
         <>
@@ -16,18 +17,12 @@ export const HomeScreen = () => {
                     <ItemCarousel text="Somos especialistas en soluciones efectivas y prácticas. Buscando, mediante razonamiento lógico, soluciones a corto, mediano y largo plazo." />
                 </Carousel.Item>
             </Carousel>
-            {/* <Carousel data-bs-theme="dark">
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://placehold.jp/24/212121/000000/900x300.png?text=&nbsp;"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-                        <h5>First slide label 1</h5>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel> */}
+
+            <div className='divisor'></div>
+
+            <div className="considerations">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis dolor neque esse. Laborum minima culpa aperiam magni mollitia rem, nesciunt libero inventore praesentium magnam dolorum perferendis repudiandae repellendus explicabo earum.
+            </div>
         </>
     )
 }
